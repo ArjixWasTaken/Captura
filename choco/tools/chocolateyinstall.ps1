a59983f37e8a261240e399b6d0c0a54f82c7bf1f
ArjixWasTaken <53124886+ArjixWasTaken@users.noreply.github.com>
2022-08-17 12:55:07 -0700

    Update chocolateyinstall.ps1

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$params = @{
     'PackageName' = 'Captura';
-    'Url' = "https://github.com/MathewSachin/Captura/releases/download/$tag/Captura-Portable.zip";
+    'Url' = "https://github.com/ArjixWasTaken/Captura/releases/download/$tag/Captura-Portable.zip";
     'UnzipLocation' = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
     'Checksum' = $checksum;
     'ChecksumType' = 'sha256';
